a0a1cba200d468605817a7cbd7933a94efb161bf
Thomiceqx <tomassdvorakk@gmail.com>
2024-09-10 10:04:17 +0200

    Odevzdání ukázky

---
+++ 00-opakovani-00-ukazka/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_opakovani_00_ukazka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
