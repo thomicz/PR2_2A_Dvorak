7b76b7a5bf1618e4d984ad61811d7d6c72ffe4f2
Thomiceqx <tomassdvorakk@gmail.com>
2024-09-15 15:53:54 +0200

    Úlohy

--- 00-opakovani-27-vypis_podle_znaku/Program.cs
+++ 00-opakovani-27-vypis_podle_znaku/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char[,] znaky = new char[,]
+            {
+    { 'a', 'b', 'c', 'd', 'e', 'f' },
+    { 'g', 'h', 'i', 'j', 'k', 'l' },
+    { 'm', 'n', 'o', 'p', 'q', 'r' },
+    { 't', 'u', 'v', 'w', 'x', 'y' }
+            };
+
+            char input;
+            do
+            {
+                Console.WriteLine($"Napiš, zda chceš vypsat řádek (r) nebo sloupec (s)");
+                input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+            while (input != 'r' && input != 's');
+
+            string tmp;
+            if (input == 'r')
+            {
+                int radek;
+                do
+                {
+                    Console.WriteLine($"Napiš číslo řádku: (1 - {znaky.GetLength(0)})");
+                    tmp = Console.ReadLine();
+                }
+                while (!int.TryParse(tmp, out radek) || radek > znaky.GetLength(0) || radek < 1);
+
+                Console.WriteLine();
+
+                for (int i = 0; i < znaky.GetLength(1); i++)
+                {
+                    Console.Write(znaky[radek - 1, i]);
+                }
+            }
+            else
+            {
+                int sloupec;
+                do
+                {
+                    Console.WriteLine($"Napiš číslo sloupce: (1 - {znaky.GetLength(1)})");
+                    tmp = Console.ReadLine();
+                }
+                while (!int.TryParse(tmp, out sloupec) || sloupec > znaky.GetLength(1) || sloupec < 1);
+
+                Console.WriteLine();
+
+                for (int i = 0; i < znaky.GetLength(0); i++)
+                {
+                    Console.Write(znaky[i, sloupec - 1]);
+                }
+            }
         }
     }
 }
---
+++ 00-opakovani-28-nejvic_nul/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _00_opakovani_28_nejvic_nul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            int[,] cisla =
+        {
+            { 0,  0, 12, 0,  7},
+            {13,  0,  5, 0,  1},
+            { 0, -1,  0, 4,  5},
+            { 1, -1,  0, 0, -5},
+        };
+
+            Console.WriteLine(program.NejvicNul(cisla, true));
+        }
+        public int NejvicNul(int[,] cisla, bool typ)
+        {
+            int maxNumber = 0;
+            int maxIndex = 0;
+
+            if (typ == false) 
+            {
+                int[] pocetNul = new int[cisla.GetLength(0)];
+                int nuly = 0;
+
+                for (int i = 0; i < cisla.GetLength(0); i++)
+                {
+                    for (int j = 0; j < cisla.GetLength(1); j++)
+                    {
+                        if (cisla[i, j] == 0)
+                        {
+                            nuly++;
+                        }
+                    }
+                    pocetNul[i] = nuly;
+                    nuly = 0;
+                }
+
+                for (int i = pocetNul.Length - 1; i >= 0; i--)
+                {
+                    if (pocetNul[i] >= maxNumber)
+                    {
+                        maxNumber = pocetNul[i];
+                        maxIndex = i;
+                    }
+                }
+                return maxIndex;
+            }
+            else
+            {
+                int[]pocetNul = new int[cisla.GetLength(1)];
+                int nuly = 0;
+
+                for (int i = 0; i < cisla.GetLength(1); i++)
+                {
+                    for (int j = 0; j < cisla.GetLength(0); j++)
+                    {
+                        if (cisla[j, i] == 0)
+                        {
+                            nuly++;
+                        }
+                    }
+                    pocetNul[i] = nuly;
+                    nuly = 0;
+                }
+
+                for (int i = pocetNul.Length - 1; i >= 0; i--)
+                {
+                    if (pocetNul[i] >= maxNumber)
+                    {
+                        maxNumber = pocetNul[i];
+                        maxIndex = i;
+                    }
+                }
+                return maxIndex;
+            }
+            
+        }
+
+    }
+}
