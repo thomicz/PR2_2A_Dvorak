9eab5bec03d23e59f561bc65685d53fbad13d7aa
Thomiceqx <tomassdvorakk@gmail.com>
2024-09-15 14:23:40 +0200

    cviceni

---
+++ 00-opakovani-26-nacitani_sudych/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _00_opakovani_26_nacitani_sudych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int n;
+            int number;
+
+            do
+            {
+                Console.WriteLine($"Zadej kolik čísel budeš zadávat: ");
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out n) || n <= 0);
+
+            int[] pole = new int[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Zadej celé číslo: ");
+                    input = Console.ReadLine();
+                }
+                while (!int.TryParse(input, out number) || number % 2 == 1);
+
+                pole[i] = number;
+            }
+
+            Console.WriteLine();
+            for (int i = pole.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{pole[i]} ");
+            }
+        }
+    }
+}
---
+++ 00-opakovani-27-vypis_podle_znaku/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_opakovani_27_vypis_podle_znaku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
