e513e7022516a39bd4bd2d9d2e827e847568319b
Thomiceqx <tomassdvorakk@gmail.com>
2024-10-01 09:14:02 +0200

    Test

---
+++ 00-Rev-01-posledni-dlouhe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata;
+
+namespace _00_Rev_01_posledni_dlouhe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 4;
+            string[] pole = { "dobry", "den", "jak", "se", "dnes", "mate", "?"};
+            Console.WriteLine($"{PosledniDlouhe(pole, limit)}");
+
+        }
+        public static string PosledniDlouhe(string[] poleRetezcu, int limit)
+        {
+            for (int i = poleRetezcu.Length - 1; i >= 0; i--)
+            {
+                if (poleRetezcu[i].Length >= limit)
+                {
+                    return poleRetezcu[i];
+                }
+            }
+            return "";
+        }
+    }
+}
---
+++ 00-Rev-02-prumer-lichych/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _00_Rev_02_prumer_lichych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            int cislo;
+            double  soucetLichychKladnych = 0;
+            double pocetLichychKladnych = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Zadej číslo: ");
+                value = Console.ReadLine();
+
+                if (value == "stop")
+                {
+                    break;
+                }
+
+                int.TryParse(value, out cislo);
+
+                if (cislo > 0 && cislo % 2 == 1)
+                {
+                    soucetLichychKladnych += cislo;
+                    pocetLichychKladnych++;
+                }
+            }
+            
+            if (pocetLichychKladnych == 0)
+            {
+                Console.WriteLine("Nebylo vloženo žádné kladné liché číslo");
+            }
+            else
+            {
+                Console.WriteLine($"Průměr: {soucetLichychKladnych / pocetLichychKladnych}");
+            }
+        
+        
+        }
+    }
+}
