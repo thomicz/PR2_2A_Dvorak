926d8b1596de843f7915a25422b6ff4ad3f5626e
Tomáš Dvořák <168711786+Thomiceqx@users.noreply.github.com>
2025-01-22 19:41:18 +0100

    Delete zkouska02 directory

--- zkouska02/Counter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace zkouska02
-{
-    internal class Counter
-    {
-        public int Count { get; protected set; }
-        public virtual void Next() => Count++;
-        public virtual void Reset() => Count = 0;
-    }
-}
